60d78705212bd6a316405e877907bbac30919bbf
Erik Uhlmann <erikuhlmann@gmail.com>
2017-01-01 02:55:07 -0500

    Make camera feed not use broadcast

--- src/org/ligerbots/robot/subsystems/VisionSubsystem.java
+++ src/org/ligerbots/robot/subsystems/VisionSubsystem.java
@@ -2,19 +2,15 @@ package org.ligerbots.robot.subsystems;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.Inet6Address;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
-import java.net.InterfaceAddress;
-import java.net.NetworkInterface;
-import java.net.SocketAddress;
 import java.net.SocketException;
-import java.util.Enumeration;
 
 import org.ligerbots.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.networktables.ConnectionInfo;
+import edu.wpi.first.wpilibj.networktables.NetworkTablesJNI;
 
 /**
  *
@@ -39,76 +35,58 @@ public class VisionSubsystem extends Subsystem implements Runnable {
 	public void initDefaultCommand() {
 	}
 
-	public InetAddress findUsb1() {
-		try {
-			Enumeration<NetworkInterface> allInterfaces = NetworkInterface.getNetworkInterfaces();
-			while (allInterfaces.hasMoreElements()) {
-				NetworkInterface networkInterface = allInterfaces.nextElement();
-				if (networkInterface.getName().equals("usb1") && networkInterface.isUp()) {
-					for (InterfaceAddress addr : networkInterface.getInterfaceAddresses()) {
-						if (addr == null)
-							continue;
-						InetAddress usb1Addr = addr.getAddress();
-						if (usb1Addr == null)
-							continue;
-						if (usb1Addr instanceof Inet6Address)
-							continue;
-						return usb1Addr;
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
 	@SuppressWarnings("resource")
 	@Override
 	public void run() {
-		InetAddress usb1Addr = null;
-		System.out.println("Finding usb1 address...");
-		while (usb1Addr == null) {
-			usb1Addr = findUsb1();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("Found usb1 address: " + usb1Addr.toString());
-		DatagramSocket usb1Sock = null, send = null;
-		SocketAddress sendAddress = null;
-		InetAddress me = null;
+		DatagramSocket udpSocket = null;
+		InetSocketAddress sendAddress = null;
 		try {
-			usb1Sock = new DatagramSocket(null);
-			usb1Sock.setReuseAddress(true);
-			usb1Sock.setBroadcast(true);
-			usb1Sock.bind(new InetSocketAddress(/* usb1Addr, */5810));
-			send = new DatagramSocket(null);
-			send.setReuseAddress(true);
-			send.setBroadcast(true);
-			send.bind(new InetSocketAddress(5809));
-			me = InetAddress.getByName("10.28.77.2");
-			sendAddress = new InetSocketAddress(InetAddress.getByName("10.28.77.255"), 5810);
+			udpSocket = new DatagramSocket(null);
+			udpSocket.setReuseAddress(true);
+			udpSocket.bind(new InetSocketAddress(5810));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		System.out.println("Begin forwarding");
 		DatagramPacket packet = null;
 		try {
-			packet = new DatagramPacket(new byte[usb1Sock.getReceiveBufferSize()],
-					usb1Sock.getReceiveBufferSize());
+			packet = new DatagramPacket(
+					new byte[udpSocket.getReceiveBufferSize()],
+					udpSocket.getReceiveBufferSize());
 		} catch (SocketException e1) {
 			e1.printStackTrace();
 		}
+
+		long lastFeedbackTime = System.currentTimeMillis();
+		DatagramPacket feedbackPacket = new DatagramPacket(
+				new byte[] { 'O', 'K' }, 2);
+
 		while (true) {
 			try {
-				usb1Sock.receive(packet);
-				if (packet.getAddress().equals(me))
-					continue;
-				packet.setSocketAddress(sendAddress);
-				send.send(packet);
+				// steal the driver laptop's IP from networktables
+				if (sendAddress == null) {
+					ConnectionInfo[] connections = NetworkTablesJNI
+							.getConnections();
+					for (ConnectionInfo connInfo : connections) {
+						if (connInfo.remote_id.equals("Android"))
+							continue;
+						sendAddress = new InetSocketAddress(connInfo.remote_ip,
+								5810);
+					}
+				}
+
+				udpSocket.receive(packet);
+
+				if (System.currentTimeMillis() - lastFeedbackTime > 1000) {
+					lastFeedbackTime = System.currentTimeMillis();
+					feedbackPacket.setSocketAddress(packet.getSocketAddress());
+					udpSocket.send(feedbackPacket);
+				}
+
+				if (sendAddress != null) {
+					packet.setSocketAddress(sendAddress);
+					udpSocket.send(packet);
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
